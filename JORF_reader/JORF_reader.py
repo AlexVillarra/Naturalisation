a495cec221185f816df0527dcf54b46391847ee6
Alejandro Villarreal Larrauri <alejandro.villareal@tractebel.engie.com>
2021-07-07 17:31:28 +0200

    Added import to __init__ file

--- JORF_reader/JORF_reader.py
+++ JORF_reader/JORF_reader.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from genericpath import isfile
 import os,sys
 import dateparser
---
+++ JORF_reader/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from .JORF_reader import JORF_Reader
